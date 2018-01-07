96e68a6a1d1e8708d62325d4b5d479271afc2d21
unknown <you@example.com>
2018-01-07 22:04:00 +0200

    added routes for all components

--- shoppingwebapp/src/app/app.module.ts
+++ shoppingwebapp/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
+import { FlashMessagesModule } from 'angular2-flash-messages/module';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -11,9 +13,23 @@ import { SummaryTopupsComponent } from './components/summary-topups/summary-topu
 import { SummaryOrdersComponent } from './components/summary-orders/summary-orders.component';
 import { SummaryCustomersComponent } from './components/summary-customers/summary-customers.component';
 import { DiscountSettingsComponent } from './components/discount-settings/discount-settings.component';
-import { SummaryAllTopupsComponent } from './components/summary-all-topups/summary-all-topups.component';
-import { SummaryAllOrdersComponent } from './components/summary-all-orders/summary-all-orders.component';
+import { ProductComponent } from './components/product/product.component';
+import { HomeComponent } from './components/home/home.component';
+import { CartComponent } from './components/cart/cart.component';
 
+const appRoutes: Routes = [
+  { path: '', component: LoginComponent, pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'register', component: RegisterComponent, pathMatch: 'full' },
+  { path: 'topup', component: TopupComponent, pathMatch: 'full' },
+  { path: 'summarytopups', component: SummaryTopupsComponent, pathMatch: 'full' },
+  { path: 'summaryorders', component: SummaryOrdersComponent, pathMatch: 'full' },
+  { path: 'summarycustomers', component: SummaryCustomersComponent, pathMatch: 'full' },
+  { path: 'settings', component: DiscountSettingsComponent, pathMatch: 'full' },
+  { path: 'product', component: ProductComponent, pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, pathMatch: 'full' },
+  { path: 'cart', component: CartComponent, pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
@@ -26,12 +42,15 @@ import { SummaryAllOrdersComponent } from './components/summary-all-orders/summa
     SummaryOrdersComponent,
     SummaryCustomersComponent,
     DiscountSettingsComponent,
-    SummaryAllTopupsComponent,
-    SummaryAllOrdersComponent
+    ProductComponent,
+    HomeComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
